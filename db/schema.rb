4abc6279c522ff4d1a40c41777607cae565cf3a4
miyahbodnar <solomiyabodnar@gmail.com>
2019-12-07 14:53:38 +0000

    added comments and design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_132405) do
+ActiveRecord::Schema.define(version: 2019_12_07_141900) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
